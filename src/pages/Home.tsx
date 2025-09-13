739447126f39f1f3b5b7520d480935c325ec81a0
tgvie <166875380+tgvie@users.noreply.github.com>
2025-09-13 04:40:18 +0200

    remove h1 in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,10 @@ import Search from "../components/Search";
 
 export default function Home() {
   return (
-    <div>
-      <h1>TechStart</h1>
-      <p>Där juniorer blir morgondagens experter!</p>
+    <>
+      <p>*Hero image* Där juniorer blir morgondagens experter!</p>
       <Dashboard />
       <Search />
-    </div>
+    </>
   );
 }
